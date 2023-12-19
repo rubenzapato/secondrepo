47a5aaab4a366d08250a94637003e35a07ad1fee
rubenzapato <128716240+rubenzapato@users.noreply.github.com>
2023-12-19 10:06:04 -0600

    change

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const menu_nav=document.querySelector("aside")
 
 function addClassShow(){
     menu_nav.classList.toggle("show")
+    console.log("click")
 }
 
 menuIcon.addEventListener("click",addClassShow)
